194c335564f115cdad7bb7dcfc9cb7704a00fda2
Lukáš Moravec <moravec.l@outlook.cz>
2021-06-11 17:28:57 +0200

    Full program bez restartu

--- Piskvorky/piskvorky.py
+++ Piskvorky/piskvorky.py
@@ -43,6 +43,27 @@ def update_pole ():
                 pygame.draw.line( screen, BARVA_KRIZKU, (sloupec * VELIKOST_POLICKA + MEZERA, (radek + 1) * VELIKOST_POLICKA - MEZERA), ((sloupec + 1) * VELIKOST_POLICKA - MEZERA, radek * VELIKOST_POLICKA + MEZERA), KRIZEK_T)
                 pygame.draw.line( screen, BARVA_KRIZKU, (sloupec * VELIKOST_POLICKA + MEZERA, radek * VELIKOST_POLICKA + MEZERA), ((sloupec + 1) * VELIKOST_POLICKA - MEZERA, (radek + 1) * VELIKOST_POLICKA - MEZERA), KRIZEK_T)
 
+def zkontroluj_vyhru(hrac):
+    for radek in range(3):
+        if pole[radek][0] == hrac and pole[radek][1] == hrac and pole[radek][2] == hrac:
+            draw_vyhra_vodorovna(radek, hrac)
+            return True
+
+    for sloupec in range(3):
+        if pole[0][sloupec] == hrac and pole[1][sloupec] == hrac and pole[2][sloupec] == hrac:
+            draw_vyhra_svisla(sloupec, hrac)
+            return True
+    
+    if pole[0][0] == hrac and pole[1][1] == hrac and pole[2][2] == hrac:
+        draw_uhlopricka_L(hrac)
+        return True
+
+    if pole[2][0] == hrac and pole[1][1] == hrac and pole[0][2] == hrac:
+        draw_uhlopricka_P(hrac)
+        return True
+
+    return False
+
 def oznac_policko(radek, sloupec, hrac):
     pole[radek][sloupec] = hrac
 
@@ -55,18 +76,53 @@ def je_pole_zaplnene():
             if pole[radek][sloupec] == 0:
                 return False
     return True
+
+def draw_vyhra_vodorovna(radek, hrac):
+    if hrac == 1:
+        barva = BARVA_KOLECKA
+    else:
+        barva = BARVA_KRIZKU
     
+    radekY = radek * VELIKOST_POLICKA + VELIKOST_POLICKA//2
+
+    pygame.draw.line(screen, barva, (15, radekY), (SIRKA - 15, radekY), SIRKA_CARY)
+
+def draw_vyhra_svisla(sloupec, hrac):
+    if hrac == 1:
+        barva = BARVA_KOLECKA
+    else:
+        barva = BARVA_KRIZKU
+
+    sloupecX = sloupec * VELIKOST_POLICKA + VELIKOST_POLICKA//2
+    pygame.draw.line(screen, barva, (sloupecX, 15), (sloupecX, VYSKA - 15), SIRKA_CARY)
+
+def draw_uhlopricka_L(hrac):
+    if hrac == 1:
+        barva = BARVA_KOLECKA
+    else:
+        barva =  BARVA_KRIZKU
+    
+    pygame.draw.line(screen, barva, (15,15), (SIRKA - 15, VYSKA - 15), SIRKA_CARY)
+
+def draw_uhlopricka_P(hrac):
+    if hrac == 1:
+        barva = BARVA_KOLECKA
+    else:
+        barva =  BARVA_KRIZKU
+    
+    pygame.draw.line(screen, barva, (15, VYSKA - 15), (SIRKA - 15, 15), SIRKA_CARY)
 
 #Main loop
 priprav_pole()
 hrac = 1
+vyhra = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and not vyhra:
             mysX = event.pos[0]
             mysY = event.pos[1]
 
@@ -74,12 +130,16 @@ while True:
             radek = int(mysY // VELIKOST_POLICKA)
 
             if je_volne_policko(radek, sloupec):
-                if hrac == 1:
-                    oznac_policko(radek, sloupec, hrac)
-                    hrac = 2
-                elif hrac == 2:
-                    oznac_policko(radek, sloupec, hrac)
-                    hrac = 1
+                oznac_policko(radek, sloupec, hrac)
+                if zkontroluj_vyhru(hrac):
+                    vyhra = True
+                
+
+
+                
+                hrac = hrac % 2 + 1
+
+              
 
 
     update_pole()
